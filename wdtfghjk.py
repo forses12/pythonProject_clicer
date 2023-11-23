33f51c05d1475e9b9ff747d105c6361159e04031
vassi <asrtjlbv@ffgh.com>
2023-11-23 18:44:33 +0300

    bruh

---
+++ wdtfghjk.py
@@ -0,0 +1,24 @@
+import random
+
+j=12345678998
+m=len(str(j))//3
+print(m)
+l=['k','m','ml','t','tl']
+n=''
+b=[]
+
+
+
+
+
+def lol():
+    for s in range(0,m):
+        kol()
+        print(b)
+def kol():
+    global j
+    x=j%1000
+    b.append(x)
+    j//=1000
+
+lol()
